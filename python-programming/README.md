f8c58f99994d7793d9d96b1833b84010af43509d
Victor Amonde <victor.amonde@gmail.com>
2024-02-27 10:43:58 +0300

    added first project

---
+++ python-programming/README.md
@@ -0,0 +1,2 @@
+# Python is a high-level programming language designed to be highly readable. 
+It uses English like statements and keywords to create a software. It is a multipurpose programming language in that it can be used for web development, creating mobile applications, hacking, automation, machine learning and data science. 
---
+++ python-programming/user_input.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+a program that asks the user for their name, age, 
+and location and then prints out a personalized message
+"""
+
+name = input("What is your name? ")
+age = input("how old are you? ")
+location = input("What is your location? ")
+print("Hello "+ name + ", you are "+ age + " years old and live in "+ location+ ".")
